b9b730e03faaf682e7037039ae0f50ccdf2237c9
Sauirbay <sAuirbau>
2025-03-13 06:06:49 +0500

    Initial Commit

---
+++ MVC/MVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVCApp.swift
+//  MVC
+//
+//  Created by Сауирбай Сейдулла on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct MVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
